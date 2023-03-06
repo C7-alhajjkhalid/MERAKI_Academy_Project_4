86d4d72972ce123c7c5e419177319b72d0950e7c
alhajjkhalid <khaled.alhajj@gmail.com>
2023-03-06 18:49:43 +0300

    finished users and subreddit schemas

---
+++ backend/models/Subreddit.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const subredditSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+    maxlength: 30,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 500,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  members: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  }],
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
---
+++ backend/models/Users.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+    maxlength: 20,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
